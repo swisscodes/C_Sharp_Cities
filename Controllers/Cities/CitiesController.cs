3a7f20612d93ceabecce9e32944fdc0607d88991
Obafemi Lawal <swissbobo@gmail.com>
2023-07-31 08:49:35 +0300

    cities init

---
+++ Controllers/Cities/CitiesController.cs
@@ -0,0 +1,27 @@
+﻿using CityInfo.Models.Cities;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CityInfo.Controllers.Cities
+{
+    [ApiController]
+    [Route("api/cities")]
+    public class CitiesController : ControllerBase
+    {
+        [HttpGet]
+        public ActionResult<IEnumerable<CityDto>> GetCities()
+        {
+            return Ok(CitiesDataStore.Current.Cities);
+        }
+        [HttpGet("{id}")]
+        public ActionResult<CityDto> GetCity(int id)
+        {
+            var resultToReturn = CitiesDataStore.Current.Cities.FirstOrDefault(c => c.Id == id);
+            if (resultToReturn == null)
+            {
+                return NotFound();
+            }
+            Console.WriteLine("hello world 1");
+            return Ok(resultToReturn);
+        }
+    }
+}
---
+++ Controllers/Cities/PointsOfInterestController.cs
@@ -0,0 +1,18 @@
+﻿using CityInfo.Models.Cities;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CityInfo.Controllers.Cities
+{
+    [Route("api/cities/{cityId}/pointsofinterest")]
+    [ApiController]
+    public class PointsOfInterestController : ControllerBase
+    {
+        [HttpGet]
+        public ActionResult<IEnumerable<PointOfInterestDto>> GetPointsOfInterest(int cityId) 
+        { 
+            var city = CitiesDataStore.Current.Cities.FirstOrDefault(c => c.Id == cityId);
+            if (city == null) return NotFound();
+            return Ok(city.PointsOfInterest);
+        }
+    }
+}
